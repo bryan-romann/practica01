85f5249cc245d70e4676c84640d446c164c52112
ArianaSilva18 <157047044+ArianaSilva18@users.noreply.github.com>
2025-06-21 00:53:16 -0600

    Revert "Se creó el controller, service, repository y domain"

--- src/main/java/com/practica01/repositorio/ArbolRepository.java
+++
@@ -1,8 +0,0 @@
-
-package com.practica01.repositorio;
-import com.practica01.domain.Arbol;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArbolRepository extends JpaRepository<Arbol, Integer> {
-
-}
